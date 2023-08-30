0854fa78d256db4b57d16af2e7a78e59b9818fd3
Diane Woodbridge <dwoodbridge@usfca.edu>
2023-08-30 08:29:54 -0700

    added Week2

---
+++ Week2/Week2-psycopg.py
@@ -0,0 +1,47 @@
+import psycopg
+from user_input import *
+
+with psycopg.connect(host=host,
+                     port=port,
+                     dbname=database,
+                     user=user) as conn:
+    with conn.cursor() as cur:
+
+        query = f'''SELECT * 
+                    FROM epa_site_location 
+                    WHERE site_id = {site_id}'''
+        cur.execute(query)
+        cur.fetchall()
+        cur.fetchall()
+
+        # If your queries include updates such 
+        # as insert, delete, or update, 
+        # you need to commit (no autocommit) or  to cancel,  rollback
+        # Increment site_id by 1 for "site_name".
+        query = f'''SELECT site_id 
+                    FROM epa_site_location 
+                    WHERE  site_name = \'{site_name}\''''
+        cur.execute(query)
+        data = cur.fetchall()
+        sid_list = []
+        for sid in data:
+            sid_list.append(sid[0])
+        for sid in sid_list:
+            query = f'''UPDATE epa_site_location 
+                        SET site_id = {site_id + 1} 
+                        WHERE site_id = {sid}'''
+            cur.execute(query)
+
+        query = f'''SELECT site_id 
+                    FROM epa_site_location 
+                    WHERE  site_name = \'{site_name}\''''
+        cur.execute(query)
+        data = cur.fetchall()
+        conn.commit()
+
+        query = f'''SELECT site_id 
+                    FROM epa_site_location 
+                    WHERE  site_name = \'{site_name}\''''
+        cur.execute(query)
+        data = cur.fetchall()
+        print(data)
